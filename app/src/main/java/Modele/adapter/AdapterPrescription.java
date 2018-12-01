4d405a42cf7d5c1a47e99455b0955214806b9b26
Chakib-Temal <chakibtemal@gmail.com>
2018-12-01 22:43:07 +0100

    V0.0.1.8

--- app/src/main/java/Modele/adapter/AdapterPrescription.java
+++ app/src/main/java/Modele/adapter/AdapterPrescription.java
@@ -6,7 +6,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.CompoundButton;
 import android.widget.LinearLayout;
+import android.widget.Switch;
 import android.widget.TextView;
 
 import com.chakibtemal.fr.patientfollow.R;
@@ -45,12 +47,18 @@ public class AdapterPrescription extends BaseAdapter {
     public View getView(int i, View view, ViewGroup viewGroup) {
         LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         View root = inflater.inflate(R.layout.item_prescription, null);
-        Prescription prescription = (Prescription) getItem(i);
+        final Prescription prescription = (Prescription) getItem(i);
 
-        LinearLayout body = (LinearLayout) root.findViewById(R.id.body);
+        final LinearLayout body = (LinearLayout) root.findViewById(R.id.body);
         TextView idPrescription = (TextView) root.findViewById(R.id.idPrescription);
         TextView drugsOfPresritpion = (TextView) root.findViewById(R.id.listDrug);
-
+        Switch simpleSwitch = (Switch) root.findViewById(R.id.simpleSwitch);
+        simpleSwitch.setFocusable(false);
+        if(prescription.isActivated()){
+            simpleSwitch.setChecked(true);
+        }else {
+            simpleSwitch.setChecked(false);
+        }
 
         idPrescription.setText(Integer.toString(i+1));
         String allDrugs = "";
@@ -61,7 +69,25 @@ public class AdapterPrescription extends BaseAdapter {
 
         if (prescription.isActivated()){
             body.setBackgroundColor(context.getResources().getColor(R.color.colorClearBlue));
+        }else {
+            body.setBackgroundColor(context.getResources().getColor(R.color.DefaultColor));
         }
+
+
+        simpleSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                // do something, the isChecked will be
+                // true if the switch is in the On position
+                if (isChecked){
+                    prescription.setActivated(true);
+                    body.setBackgroundColor(context.getResources().getColor(R.color.colorClearBlue));
+                }else {
+                    prescription.setActivated(false);
+                    body.setBackgroundColor(context.getResources().getColor(R.color.DefaultColor));
+                }
+            }
+        });
+
 
         return root;
     }
